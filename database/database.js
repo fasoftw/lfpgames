097903ab6798c284efc21600f1e3cafed3705807
debora-cristina <deboracrisf91@gmail.com>
2020-09-19 11:59:42 -0300

    Débora - comentando produção

--- database/database.js
+++ database/database.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize ('lfp', 'fasoft', 'Friden8781',{
-    host: 'mysql669.umbler.com',
+// const connection = new Sequelize ('lfp', 'fasoft', 'Friden8781',{
+//     host: 'mysql669.umbler.com',
+//     dialect: 'mysql',
+//     timezone: "-03:00" //define timezone no sequelize
+// });
+
+const connection = new Sequelize ('lfpgames', 'root', 'root',{
+    host: 'localhost',
     dialect: 'mysql',
     timezone: "-03:00" //define timezone no sequelize
 });
